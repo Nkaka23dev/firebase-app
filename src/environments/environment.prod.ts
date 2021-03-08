e61d78d42de8f5dba89a28a6170a112fc39f7c7e
Nkaka23dev <62412678+Nkaka23dev@users.noreply.github.com>
2021-03-08 17:17:22 +0200

    generating all components needed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCragGlXNmrglZKuUpegF2SbMbsVLoOaQ0",
+    authDomain: "nkaka-b76a2.firebaseapp.com",
+    projectId: "nkaka-b76a2",
+    storageBucket: "nkaka-b76a2.appspot.com",
+    messagingSenderId: "1017864537522",
+    // appId: "1:1017864537522:web:f8371be7b9737e4855d0ab",
+    // measurementId: "G-9CDQZ5W6L6"
+  }
+  }
